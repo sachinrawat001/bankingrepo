2a41ff1b80c3128776096b29e127e3031dbaa8df
sachi <sachi@SACHINRAWAT>
2025-02-19 11:12:42 +0530

    first commit

---
+++ gatewaybatch5/src/main/java/com/gatewaybatch5/Gatewaybatch5Application.java
@@ -0,0 +1,13 @@
+package com.gatewaybatch5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gatewaybatch5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gatewaybatch5Application.class, args);
+	}
+
+}
---
+++ gatewaybatch5/src/test/java/com/gatewaybatch5/Gatewaybatch5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatewaybatch5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gatewaybatch5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
